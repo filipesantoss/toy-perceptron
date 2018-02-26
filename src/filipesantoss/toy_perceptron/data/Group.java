5f11846f653d74b376aca042dcd1dc19de07d13d
filipesantoss <filipe.santos@academiadecodigo.org>
2018-02-26 22:54:23 +0000

    misunderstood bias value meaning big time. fixing it

--- src/filipesantoss/toy_perceptron/data/Group.java
+++ src/filipesantoss/toy_perceptron/data/Group.java
@@ -1,15 +1,12 @@
 package filipesantoss.toy_perceptron.data;
 
 public enum Group {
-    FIRST(0.01f),
-    SECOND(-0.01f);
+    FIRST(100f),
+    SECOND(-100f);
 
     /**
      * A numeric representation of the group used by the perceptron to calculate the error when in training.
-     * This value is arbitrary, as long as one is positive and the other is negative.
-     *
-     * Numbers with high absolute value result in high input weight oscillation during perceptron training,
-     * which may cause the perceptron to fail when categorizing input that was previously categorized correctly.
+     * This value equals the limit values for the inputs of that group.
      */
     private float value;
 
